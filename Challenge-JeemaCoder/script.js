59fc70deed9ceb852fe2a91d011064267a5dba7a
Moussa Diatta <diattamoussa010@gmail.com>
2023-10-04 01:28:51 +0000

    modifications sur le challenge JeemaCoder

--- Challenge-JeemaCoder/script.js
+++ Challenge-JeemaCoder/script.js
@@ -1,16 +1,16 @@
 function validateForm() {
-    let nom = document.getElementById("nom").value;
     let prenom = document.getElementById("prenom").value;
+    let nom = document.getElementById("nom").value;
     let email = document.getElementById("email").value;
     let telephone = document.getElementById("telephone").value;
 
-    if(nom = ""){
-        alert("Veuillez le champ nom")
+    if(prenom = ""){
+        alert("Veuillez le champ prenom")
         return false;
     }
 
-    if(prenom = ""){
-        alert("Veuillez le champ prenom")
+    if(nom = ""){
+        alert("Veuillez le champ nom")
         return false;
     }
 
@@ -22,10 +22,18 @@ function validateForm() {
     if(email = ""){
         alert("Veuillez le champ email")
         return false;
-    }
+    } 
+    // else if (!email.includes("@")){
+    //     alert("email invalide")
+    //     return false;
+    // }
+    
+    return true;
+
 }
 
-function showDate(){
+//Fonction d'affichage
+function showData(){
     let listUser;
     if(localStorage.getItem("listUser") == null){
         listUser = [];
@@ -46,4 +54,93 @@ function showDate(){
         html += "</tr>";
     });
     document.querySelector("#table tbody").innerHTML = html;
+}
+
+// Charger les données du formulaire depuis le localStorage au démarage de la page
+document.onload = showData();
+
+// Fonction ajout de données dans le localStorage
+function addData(){
+    if(validateForm() == true){
+        let prenom = document.getElementById("prenom").value;
+        let nom = document.getElementById("nom").value;
+        let email = document.getElementById("email").value;
+        let telephone = document.getElementById("telephone").value;
+        
+        let listUser;
+        if(localStorage.getItem("listUser") == null){
+            listUser = [];
+        }
+        else{
+            listUser = JSON.parse(localStorage.getItem("listUser"));
+        }
+
+        listUser.push({
+            prenom : prenom,
+            nom : nom,
+            email : email,
+            telephone : telephone,
+        });
+
+        localStorage.setItem("listUser", JSON.stringify(listUser));
+        showData();
+        document.getElementById("prenom").value = "";
+        document.getElementById("nom").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("telephone").value = "";
+    }
+}
+
+//Fonction de suppression les données dans le localStorage
+function deleteData(index){
+    let listUser;
+        if(localStorage.getItem("listUser") == null){
+            listUser = [];
+        }
+        else{
+            listUser = JSON.parse(localStorage.getItem("listUser"));
+        }
+    listUser.splice(index, 1);
+    localStorage.setItem("listUser", JSON.stringify(listUser));
+    showData();
+}
+
+//Fonction de modification
+function updateData(index){
+    document.getElementById("submit").style.display = "none";
+    document.getElementById("edit").style.display = "block";
+
+    let listUser;
+    if(localStorage.getItem("listUser") == null){
+        listUser = [];
+    }
+    else{
+        listUser = JSON.parse(localStorage.getItem("listUser"));
+    }
+
+    document.getElementById("prenom").value = listUser[index].prenom;
+    document.getElementById("nom").value = listUser[index].nom;
+    document.getElementById("email").value = listUser[index].email;
+    document.getElementById("telephone").value = listUser[index].telephone;
+
+    document.querySelector("#edit").onclick = function(){
+        if(validateForm() == true){
+            listUser[index].prenom = document.getElementById("prenom").value;
+            listUser[index].nom = document.getElementById("nom").value;
+            listUser[index].email = document.getElementById("email").value;
+            listUser[index].telephone = document.getElementById("telephone").value;
+
+            localStorage.setItem("listUser", JSON.stringify(listUser));
+            
+            showData();
+
+            document.getElementById("prenom").value = "";
+            document.getElementById("nom").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("telephone").value = "";
+
+            document.getElementById("submit").style.display = "block";
+            document.getElementById("edit").style.display = "none";
+        }
+    }
 }
